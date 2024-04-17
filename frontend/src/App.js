09a5a731fd5f8b31f96faa0b8830e3ca3240441b
Pramita0410 <sandhyan.p@northeastern.edu>
2024-04-17 11:24:03 -0400

    Frontend Uploaded

---
+++ frontend/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import {
+  BrowserRouter, Routes, Route,
+} from 'react-router-dom';
+import "./pages/Home"
+import "./index.css";
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from './reducer/index';
+import Navbar from "./components/common/Navbar"
+
+
+const store = configureStore({
+  reducer: rootReducer
+});
+
+
+function App() {
+  return (
+
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+
+
+      {/* go upward arrow */}
+
+      <BrowserRouter>
+        <Provider store={store}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+
+
+          </Routes>
+        </Provider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
